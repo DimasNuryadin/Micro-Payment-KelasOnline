c520051d109917741bec815d64d9a75612a83d84
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2025-02-09 22:03:32 +0700

    done create API webhook

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\WebhookController;
 
+// Orders
 Route::post('/orders', [OrderController::class, 'create']);
 Route::get('/orders', [OrderController::class, 'index']);
+
+// Webhook
+Route::post('/webhook', [WebhookController::class, 'midtransHandler']);
